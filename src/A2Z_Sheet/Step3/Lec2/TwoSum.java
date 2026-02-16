c34d198d7d2153d5e7bf9d6d860a815b2f7fe652
priyanshu <priyanshugangwar17@gmail.com>
2026-02-16 16:53:13 +0530

    Problem: TwoSum with hashmap is completed.

--- src/A2Z_Sheet/Step3/Lec2/TwoSum.java
+++ src/A2Z_Sheet/Step3/Lec2/TwoSum.java
@@ -1,21 +1,20 @@
 package A2Z_Sheet.Step3.Lec2;
 //Ps:1
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
     static int[] twoSum(int[] nums, int target){
-        int left = 0 , right = nums.length-1;
-        Arrays.sort(nums);
-        while (left<right){
-            int sum = nums[left] + nums[right];
-            if (sum == target){
-                return new int[]{left,right};
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)){
+                return new int[] {map.get(complement),i};
             }
-            if (sum<target) left++;
-            else right--;
+            map.put(nums[i],i);
         }
         return new int[]{-1,-1};
-    }
+     }
 
     public static void main(String[] args) {
         int[] arr = {3,2,4};

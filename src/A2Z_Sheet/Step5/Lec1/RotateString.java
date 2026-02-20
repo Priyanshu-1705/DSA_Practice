66e3e1160883ec47d4236af6c053757fbb658a1d
priyanshu <priyanshugangwar17@gmail.com>
2026-02-20 22:58:13 +0530

    Problem: Rotate String is completed.

---
+++ src/A2Z_Sheet/Step5/Lec1/RotateString.java
@@ -0,0 +1,26 @@
+package A2Z_Sheet.Step5.Lec1;
+//Ps:796
+public class RotateString {
+    static boolean rotateString(String s, String goal) {
+        //Brute force
+//        if (s.length() != goal.length()) return false;
+//        for (int i = 0; i < s.length(); i++) {
+//            String rotated = s.substring(i)+s.substring(0,i);
+//            if (rotated.equals(goal)) return true;
+//        }
+//        return false;
+
+        //Optimal approach
+//        Agar hum dono string ko add krdenge toh humara goal uss new string me present hoga
+        if (s.length() != goal.length()) return false;
+        String doublestring = s+goal;
+        return doublestring.contains(goal);
+    }
+
+
+    public static void main(String[] args) {
+        String s = "rotation";
+        String goal = "tionrota";
+        System.out.println(rotateString(s,goal));
+    }
+}

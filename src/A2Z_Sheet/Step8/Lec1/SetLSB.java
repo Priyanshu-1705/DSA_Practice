49699deebff7c3a26bea961034ee2da6fb246212
priyanshu <priyanshugangwar17@gmail.com>
2026-04-04 21:22:10 +0530

    Problem: SetLSB is completed.

---
+++ src/A2Z_Sheet/Step8/Lec1/SetLSB.java
@@ -0,0 +1,11 @@
+package A2Z_Sheet.Step8.Lec1;
+
+public class SetLSB {
+    public static void main(String[] args) {
+        int n = 7;
+
+        int ans = n | 1;
+        System.out.println(ans);
+
+    }
+}
